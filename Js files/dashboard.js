3d757b52969b5f78f432d82585da07b77da775d0
Sheni-Black <vfe05720cshefiu@gmail.com>
2026-08-20 19:49:34 -0700

    Conpleted the nav bar and the profile header for the dashboard

---
+++ Js files/dashboard.js
@@ -0,0 +1,12 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const aside = document.querySelector(".dashboard-nav");
+const closeAside = document.querySelector(".close");
+
+hamburgerMenu.addEventListener("click", ()=> {
+    aside.classList.add("aside-show")
+})
+
+closeAside.addEventListener("click", () =>{
+    hamburgerMenu.style.display = "block"
+    aside.classList.remove("aside-show")
+})
