6b1921c9f9464f7060dee067c905ecb508976c0a
Sheni-Black <vfe05720cshefiu@gmail.com>
2026-09-11 09:45:44 -0700

    I initally shown all containers in the auth file so alll pages could seen, i have gone to hide them back so all pages can only be seen when necessary

--- Js files/auth.js
+++ Js files/auth.js
@@ -46,31 +46,31 @@ const containers = [
     newPasswordSet
 ];
 
-// SHOW / HIDE AUTHENTICATION CONTAINERS
+SHOW / HIDE AUTHENTICATION CONTAINERS
 
-// function hideAllContainers() {
-//     // loop through every element of the containers array and give it a display of none
+function hideAllContainers() {
+    // loop through every element of the containers array and give it a display of none
     
-//     containers.forEach((container) => {
+    containers.forEach((container) => {
 
-//         if (container) {
-//             container.style.display = "none";
-//         }
+        if (container) {
+            container.style.display = "none";
+        }
 
-//     });
-// }
+    });
+}
 
 
-// function showContainer(container) {
+function showContainer(container) {
 
-//     hideAllContainers();
+    hideAllContainers();
 
-//     if (container) {
-//         container.style.display = "block";
-//     }
-// }
+    if (container) {
+        container.style.display = "block";
+    }
+}
 
-// INITIAL PAGE
+INITIAL PAGE
 
 showContainer(signupContainer);
 
